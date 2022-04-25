6729c76e276a1e52320457f7fac07dcd1fd269a0
Dushko Avramoski <avramoski7@gmail.com>
2022-04-25 21:35:44 +0200

    Change url

--- src/main/java/com/example/libraryapp/RestControllers/BookRestController.java
+++ src/main/java/com/example/libraryapp/RestControllers/BookRestController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping({"/api/", "/api/books"})
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://library-order-book-application.herokuapp.com/")
 public class BookRestController {
 
     private final BookService bookService;
